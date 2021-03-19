f655c27ff326f420266d93563b204584aa27e484
ahsanahmedkhan <Ahsanahmed.khan@hotmail.com>
2021-03-19 20:24:04 +0500

    solve the error

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,36 +1,37 @@
 import React, {useState, useEffect} from 'react';
 import { fetchDailyData } from '../../api';
 
-import { Line, Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 import styles from './Chart.module.css';
 
 const Chart = () => {
      const [dailyData, setDailyData] = useState([]);
+
 
      useEffect(() => {
          const fetchAPI = async () => {
              setDailyData(await fetchDailyData());
          }
 
-         console.log(dailyData);
-
          fetchAPI();
+         
      });
 
      const lineChart =(
-         dailyData.length 
+         dailyData
          ? ( <Line 
          data = {{
-             labels: dailyData(({ date }) => date ),
+             labels: dailyData.map(({ date }) => date ),
              datasets: [{
-                 data: dailyData(( { confirmed }) => confirmed),
+                 data: dailyData.map(( { confirmed }) => confirmed),
                  label: 'Infected',
                  borderColor: '#3333ff',
                  fill: true,
              }, {
-                 data: dailyData(({ deaths }) => deaths),
+                 data: dailyData.map(({ deaths }) => deaths),
                  label: 'Deaths',
                  borderColor: 'red',
+                 backgroundColor: 'rgba(255, 0, 0, 0.5',
                  fill: true,
              }],
          }}
@@ -38,7 +39,11 @@ const Chart = () => {
      );
 
   return(
-      <h1>Charts</h1>
+      <div className="styles.container">
+          {lineChart}
+
+      </div>
+
   )      
 }
 
